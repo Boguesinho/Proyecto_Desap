10f710562ea4d9b168291004dbb82b188ff0a562
Luis Fajardo <lrfs_13@hotmail.com>
2020-11-17 13:13:42 -0600

    Estructura de la app
    Estructura básica de la aplicación en android studio.

---
+++ Aplicacion/app/src/main/java/com/example/proyectodesap_intlok/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectodesap_intlok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
