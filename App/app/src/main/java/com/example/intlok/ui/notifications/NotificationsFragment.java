195fd709da938f2bd07b481ae7911918559d2c1a
Alejandro <alex-blanco-00@hotmail.com>
2020-11-20 16:10:38 -0600

    Cambios perfil

--- App/app/src/main/java/com/example/intlok/ui/notifications/NotificationsFragment.java
+++ App/app/src/main/java/com/example/intlok/ui/notifications/NotificationsFragment.java
@@ -1,11 +1,12 @@
 package com.example.intlok.ui.notifications;
-
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -31,5 +32,48 @@ public class NotificationsFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    public void mostrarAlerta(){
+        Alerta alerta = new Alerta();
+        alerta.mostrar();
+    }
+}
+
+class Alerta extends Activity {
+    /** Called when the activity is first created. */
+    AlertDialog.Builder dialogo1;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("Importante");
+        dialogo1.setMessage("¿ Acepta la ejecución de este programa en modo prueba ?");
+        dialogo1.setCancelable(false);
+        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                aceptar();
+            }
+        });
+        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                cancelar();
+            }
+        });
+
+    }
+    public void mostrar(){
+        dialogo1.show();
+    }
+
+    public void aceptar() {
+        Toast t=Toast.makeText(this,"Bienvenido a probar el programa.", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void cancelar() {
+        finish();
     }
 }
