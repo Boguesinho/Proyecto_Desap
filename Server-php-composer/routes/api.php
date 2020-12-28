6a42e97ec271425d3abeefb5737fd0d53999e64a
Luis Fajardo <lrfs_13@hotmail.com>
2020-12-27 23:22:33 -0600

    Registro y Login con JWT
    Ya se puede registrar, logearse y entrar a las rutas que necesite un token.

--- Server-php-composer/routes/api.php
+++ Server-php-composer/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+//use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,36 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', [UsuarioController::class, 'register']);
+Route::post('login', [UsuarioController::class, 'authenticate']);
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('login', [AuthController::class, 'login']);
-    
+*/
+
+/*
+Route::group(['prefix' => 'auth'], function()
+{
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+*/
+
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+
+    Route::post('user',[UsuarioController::class, 'getAuthenticatedUser']);
+
+    Route::get("ping", function(){
+         return ["message" => "pong"];
+    });
+
+});
+Route::get("home", [HomeController::class, "index"]);
+// Route::get("ping", function(){
+//     return ["message" => "pong"];
+// });
